2d8716d1b38344d1e2800d04114f5de1b0032cf6
Robert Ulmer <robert@familieulmer.info>
2023-09-07 10:42:43 +0200

    cuse in container

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+stop_container() {
+  sudo podman stop code-server || echo container not running
+  sudo podman container rm code-server || echo no container
+  return 0
+}
+
 sudo podman image build --tag vs-code-rdp-base:latest -f $(dirname $0)/Containerfile.base && \
 sudo podman image build --tag vs-code-rdp-services:latest -f $(dirname $0)/Containerfile.services && \
-sudo podman stop code-server || echo not running && \
+stop_container && \
+sudo mknod --mode=666 /dev/rdp_cdev c 234 0 
+sudo rm $(dirname $0)/workspace/logs/*.log
 sudo podman run -d --rm --name=code-server \
   -e SUDO_PASSWORD=sudo \
   -e PUID=$(id -u) \
@@ -9,7 +18,8 @@ sudo podman run -d --rm --name=code-server \
   -e TZ=Europe/Vienna \
   -p 8443:8443 \
   -p 8080:80 \
-  -v /home/robert/Development/htl/lehre/rdp/config:/config \
-  -v /home/robert/Development/htl/lehre/rdp/workspace/:/config/workspace \
+  -v $(dirname $0)/config:/config \
+  -v $(dirname $0)/workspace/:/config/workspace \
+  --device /dev/cuse \
   --device /dev/rdp_cdev \
   localhost/vs-code-rdp-services:latest 
