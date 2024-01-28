90bb033dfbdb3f58a78de6e83465bb8a93ad6180
Robert Ulmer <rulmer@lr.htlweiz.at>
2024-01-28 21:43:21 +0100

    Room management

---
+++ scripts/room.nixos.nginx.template.nix
@@ -0,0 +1,11 @@
+# HEAD{ ...}:
+# HEAD{
+# HEAD  services.nginx = {
+# MAIN    virtualHosts."HOST" = {
+# MAIN      locations."/" = {
+# MAIN        proxyPass = "http://127.0.0.1:PORT";
+# MAIN        proxyWebsockets = true;
+# MAIN      };
+# MAIN    };
+# FOOT  };
+# FOOT}
--- scripts/rooms.sh
+++ scripts/rooms.sh
@@ -19,6 +19,23 @@ stop_room() {
     podman pod rm ${pod_name}
   done
 }
+generate_room() {
+  room=$1; shift
+  TEMPLATE=${SCRIPT_DIR}/room.nixos.nginx.template.nix
+  
+  cat ${TEMPLATE} | grep -F '# HEAD' | sed -e 's/# HEAD//'
+  for workstation_line in $(get_room ${room}); do
+    workstation_domain=$(echo ${workstation_line} | cut -d , -f 2)
+    domain=$(echo ${workstation_domain}  | sed -e "s/.*\.//")
+    workstation=$(echo ${workstation_domain} | sed -e "s/\.${domain}//")
+    pod_number=$(echo ${workstation_line} | cut -d , -f 3)
+    code_port=$(( pod_number*5+8001 ))
+    app_port=$(( pod_number*5+8000 ))
+    grep -F '# MAIN' ${TEMPLATE} | sed -e 's/# MAIN//' | sed -e s/HOST/${workstation}-code.${domain}/ | sed s/PORT/${code_port}/
+    grep -F '# MAIN' ${TEMPLATE} | sed -e 's/# MAIN//' | sed -e s/HOST/${workstation}-app.${domain}/ | sed s/PORT/${app_port}/
+  done
+  cat ${TEMPLATE} | grep -F '# FOOT' | sed -e 's/# FOOT//'
+}
 main() {
   cmd=$1;  shift; [ -z "${cmd}"  ] && usage
   room=$1; shift; [ -z "${room}" ] && usage
@@ -35,6 +52,10 @@ main() {
       ;;
     status)
       info checking status of room ${room} 
+      ;;
+    generate)
+      info generating nixos nginx config of room ${room}
+      generate_room ${room}
       ;;
     *)
       error "unknown command ${cmd}"
@@ -71,4 +92,5 @@ usage() {
   critical 1 "$(basename $0) <cmd> <room>" 
 }
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 main $*
